dd52143610999f62e6ffd055dcdc09f592432253
Tommy031088 <tommy031088@gmail.com>
2019-08-25 00:15:32 +0800

    2019.08.25
    Changes:
    
    Tileset complete. (temporarily)
    
    Add a player with basic animation and controller script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
+    [Range(1f,10f)]
+    public float speed = 5;
+    [HideInInspector]
+    public Vector2 direction = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+        rb.velocity = direction.normalized * speed;
+        if(direction.x != 0)
+            sr.flipX = ((direction.x > 0) ? false : true);
+        anim.SetFloat("Velocity", rb.velocity.magnitude);
+    }
+}
