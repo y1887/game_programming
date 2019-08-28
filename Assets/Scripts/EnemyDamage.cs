a3d74e9f4a561a0d2be73173ed5d175c28f50e32
Tommy031088 <tommy031088@gmail.com>
2019-08-28 22:01:03 +0800

    2019.08.28
    Changes:
    
    Add the damage text.

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyDamage : MonoBehaviour
 {
     [Header("擊退力度"), Range(10f,50f)]
     public float forceMultiplier = 15f;
+    public GameObject hitText;
+    [Header("字體跳躍高度"), Range(1f, 5f)]
+    public float jumpMultiplier = 1.5f;
+    [Header("字體跳躍時間"), Range(0.5f, 2f)]
+    public float jumpTime = 0.8f;
+    [Header("跳躍曲線")]
+    public Ease jumpEase;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Material hitMaterial, original;
@@ -23,11 +31,16 @@ public class EnemyDamage : MonoBehaviour
 
     public IEnumerator getHit(Vector2 playerPos, float time)
     {
-        Vector2 force = (new Vector2(this.transform.position.x, this.transform.position.y) - playerPos).normalized * forceMultiplier;
+        Vector2 direction = (new Vector2(this.transform.position.x, this.transform.position.y) - playerPos).normalized;
+        Vector2 force = direction * forceMultiplier;
         yield return new WaitForSeconds(time);
+        GameObject thisHitText = GameObject.Instantiate(hitText, this.transform.position, Quaternion.identity);
+        thisHitText.transform.DOJump(thisHitText.transform.position + new Vector3(direction.x, direction.y, 0), jumpMultiplier, 1, jumpTime).SetEase(jumpEase);
         rb.AddForce(force, ForceMode2D.Impulse);
         sr.material = hitMaterial;
         yield return new WaitForSeconds(0.1f);
         sr.material = original;
+        yield return new WaitForSeconds(jumpTime - 0.1f);
+        Destroy(thisHitText);
     }
 }
